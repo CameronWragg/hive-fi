d84d0612beb4b91956cc613870e40dc822d19a89
Cameron <cameron.wragg@hotmail.co.uk>
2021-12-04 19:07:06 +0000

    Adding bootlogo image, script and service

---
+++ bootlogo/hive-fi-bootlogo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from sys import exit
+
+from PIL import Image
+import ST7789 as ST7789
+
+
+def main() -> int:
+    try:
+        disp = ST7789.ST7789(
+            height=240,
+            rotation=90,
+            port=0,
+            cs=ST7789.BG_SPI_CS_FRONT, 
+            dc=9,
+            backlight=19,
+            spi_speed_hz=80 * 1000 * 1000,
+            offset_left=0,
+            offset_top=0
+        )
+
+        WIDTH = disp.width
+        HEIGHT = disp.height
+
+        disp.begin()
+        image = Image.open("/home/pi/hive-fi/bootlogo/hive-fi.jpeg")
+        image = image.resize((WIDTH, HEIGHT))
+        disp.display(image)
+        return 0
+
+    except Exception:
+        return 1
+
+
+if __name__ == "__main__":
+    exit(main())
