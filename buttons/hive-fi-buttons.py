fdf0f0ab97c0a1d214bff51e5d6c9870907014ee
Cameron <cameron.wragg@glencore.co.uk>
2021-12-08 17:09:26 +0000

    Adding shutdown and reboot functions

--- buttons/hive-fi-buttons.py
+++ buttons/hive-fi-buttons.py
@@ -1,33 +1,62 @@
 import signal
+from os import system
+from sys import exit
+
 import RPi.GPIO as GPIO
 
 
-BUTTONS = [5, 6, 16, 24]
-LABELS = ['A', 'B', 'X', 'Y']
+def main() -> int:
+    try:
+        BUTTONS = [5, 6, 16, 24]
+        LABELS = ['A', 'B', 'X', 'Y']
+        BL_CYCLE = [50, 75, 100, 0, 25]
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(13, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(13, GPIO.OUT)
 
-B_CYCLE = [0, 25, 50, 75, 100]
-BACKLIGHT = GPIO.PWM(13, 500)
-BACKLIGHT.start(100)
+        BACKLIGHT = GPIO.PWM(13, 500)
+        BACKLIGHT.start(25)
 
-def default_behaviour(pin):
-    label = LABELS[BUTTONS.index(pin)]
-    print(f"Button press detected on pin: {pin} label: {label}")
+        def default_behaviour(pin):
+            label = LABELS[BUTTONS.index(pin)]
+            print(f"{label} button pressed, no action currently assigned.")
 
-def backlight_change(pin) -> None:
-    label = LABELS[BUTTONS.index(pin)]
-    _next_b_cycle = B_CYCLE.pop(0)
-    print(f"Button press detected on {label}, changing backlight brightness to {_next_b_cycle}.")
-    
-    B_CYCLE.append(_next_b_cycle)
-    BACKLIGHT.ChangeDutyCycle(_next_b_cycle)
+        def backlight_change(pin) -> None:
+            label = LABELS[BUTTONS.index(pin)]
+            _next_b_cycle = BL_CYCLE.pop(0)
+            print(f"{label} button pressed, backlight set to: {_next_b_cycle}.")
+            BL_CYCLE.append(_next_b_cycle)
+            BACKLIGHT.ChangeDutyCycle(_next_b_cycle)
 
-GPIO.add_event_detect(BUTTONS[0], GPIO.FALLING, default_behaviour, bouncetime=100)
-GPIO.add_event_detect(BUTTONS[1], GPIO.FALLING, backlight_change, bouncetime=100)
-GPIO.add_event_detect(BUTTONS[2], GPIO.FALLING, default_behaviour, bouncetime=100)
-GPIO.add_event_detect(BUTTONS[3], GPIO.FALLING, default_behaviour, bouncetime=100)
+        def safe_shutdown(pin) -> None:
+            command = "sudo shutdown now -h"
+            label = LABELS[BUTTONS.index(pin)]
+            print(f"{label} button pressed, shutting down.")
+            BACKLIGHT.stop()
+            GPIO.cleanup()
+            system(command)
 
-signal.pause()
+        def safe_reboot(pin) -> None:
+            command = "sudo shutdown now -r"
+            label = LABELS[BUTTONS.index(pin)]
+            print(f"{label} button pressed, rebooting.")
+            BACKLIGHT.stop()
+            GPIO.cleanup()
+            system(command)
+
+
+        GPIO.add_event_detect(BUTTONS[0], GPIO.FALLING, default_behaviour, bouncetime=200)
+        GPIO.add_event_detect(BUTTONS[1], GPIO.FALLING, backlight_change, bouncetime=200)
+        GPIO.add_event_detect(BUTTONS[2], GPIO.FALLING, safe_shutdown, bouncetime=200)
+        GPIO.add_event_detect(BUTTONS[3], GPIO.FALLING, safe_reboot, bouncetime=200)
+
+        signal.pause()
+
+    except Exception:
+        GPIO.cleanup()
+        return 1
+
+
+if __name__ == "__main__":
+    exit(main())
