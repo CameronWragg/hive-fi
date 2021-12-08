85a4aa48cf49f4c43a92422391afc152716ac4e4
Cameron <cameron.wragg@hotmail.co.uk>
2021-12-08 12:35:56 +0000

    Reverting PWM brightness change

--- bootlogo/hive-fi-bootlogo.py
+++ bootlogo/hive-fi-bootlogo.py
@@ -3,7 +3,6 @@ from sys import exit
 
 from PIL import Image
 from ST7789 import ST7789
-import RPi.GPIO as GPIO
 
 
 def main() -> int:
@@ -14,15 +13,11 @@ def main() -> int:
             port=0,
             cs=ST7789.BG_SPI_CS_FRONT, 
             dc=9,
-            backlight=None,
+            backlight=19,
             spi_speed_hz=80 * 1000 * 1000,
             offset_left=0,
             offset_top=0
         )
-        
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(13, GPIO.OUT)
-        backlight = GPIO.PWM(13, 500)
 
         WIDTH = disp.width
         HEIGHT = disp.height
@@ -30,7 +25,6 @@ def main() -> int:
         disp.begin()
         image = Image.open("/home/pi/hive-fi/bootlogo/hive-fi.jpeg")
         image = image.resize((WIDTH, HEIGHT))
-        backlight.start(30)
         disp.display(image)
         return 0
 
