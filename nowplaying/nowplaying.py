125e5335c2089297e8d27f84dbf42e6c514d951c
Cameron <cameron.wragg@glencore.co.uk>
2021-12-08 17:35:56 +0000

    Updating nowplaying and adding service

--- nowplaying/nowplaying.py
+++ nowplaying/nowplaying.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
-from os import environ
-from sys import exit
-from requests import get
+from os import environ, system
+from sys import argv, exit
 
 from PIL import Image
-import ST7789 as ST7789
+from requests import get
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
+from ST7789 import BG_SPI_CS_FRONT, ST7789
 
 
 def main() -> int:
     try:
+        system(f"export SPOTIPY_CLIENT_ID='{argv[1]}'")
+        system(f"export SPOTIPY_CLIENT_SECRET='{argv[2]}'")
+
         _track_id = environ.get("TRACK_ID")
-        _spotify = Spotify(auth_manager=SpotifyClientCredentials())
+        _spotify = Spotify(client_credentials_manager=SpotifyClientCredentials())
         _track = _spotify.track(_track_id)
         _res = get(_track["album"]["images"][0]['url'])
         _img = Image.open(_res.raw)
 
-        _display = ST7789.ST7789(
+        _display = ST7789(
             height=240,
             rotation=90,
             port=0,
-            cs=ST7789.BG_SPI_CS_FRONT, 
+            cs=BG_SPI_CS_FRONT, 
             dc=9,
-            backlight=19,
+            backlight=None,
             spi_speed_hz=80 * 1000 * 1000,
             offset_left=0,
             offset_top=0
@@ -37,6 +40,7 @@ def main() -> int:
         _display.display(_img)
 
         return 0
+
     except Exception:
         return 1
 
